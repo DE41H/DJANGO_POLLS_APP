3f430302c190665285c95b57ad40acba90b24477
sreyash <sreyash.s@outlook.com>
2025-11-22 01:18:54 +0530

    improved detail view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.template import loader
 from .models import Question
 
@@ -13,7 +13,8 @@ def index(request):
     return render(request, 'index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse()
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse()
